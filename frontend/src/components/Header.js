c9ca1ac363d9ed850368b532a182399357aaa6d9
jb <jasontbrown99@gmail.com>
2020-10-05 08:12:14 -0700

    add backend and add a copy of products to backend

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" varient="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart" className="mr-2">
-                <i className="fas fa-shopping-cart mr-2"></i>Cart
-              </Nav.Link>
-              <Nav.Link href="/login" className="mr-2">
-                <i className="fas fa-user mr-2"></i>Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link className="mr-2">
+                  <i className="fas fa-shopping-cart mr-2"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link className="mr-2">
+                  <i className="fas fa-user mr-2"></i>Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
