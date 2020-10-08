d20f15b7ea49d03b6bd7a9b6e8bd7d383c9e04bf
jb <jasontbrown99@gmail.com>
2020-10-08 16:37:03 -0700

    add controllers to backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,36 +1,11 @@
 const express = require("express");
-// npm package that helps with errorHandling for async requests
-const asyncHandler = require("express-async-handler");
 const router = express.Router();
-const Product = require("../models/ProductModel");
+const {
+  getProducts,
+  getProductById,
+} = require("../controllers/productController");
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    // throw new Error("Some error!");
-    res.json(products);
-  })
-);
-
-// @desc    Fetch a product
-// @route   GET /api/products/:id
-// @access  Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 module.exports = router;
