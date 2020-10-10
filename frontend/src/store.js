37bbfbfef9327e93403a9f28c003fa04daf9630a
jb <jasontbrown99@gmail.com>
2020-10-10 11:07:48 -0700

    add registerScreen and functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,13 +6,14 @@ import {
   productDetailReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
-import { userLoginReducer } from "./reducers/userReducers";
+import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetail: productDetailReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
 const getCartItemsFromStorage = localStorage.getItem("cartItems")
